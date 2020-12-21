12fc683d018f646f08339b4d6338dee0f0182571
Dashi <68922046+Dashi96@users.noreply.github.com>
2020-12-21 20:31:55 +0700

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+import { radioPlayerInit } from './radioPlayer.js'
+import { musicPlayerInit } from './musicPlayer.js'
+import { videoPlayerInit } from './videoPlayer.js'
+
+const playerBtn = document.querySelectorAll('.player-btn')
+const playeBlock = document.querySelectorAll('.player-block')
+const temp = document.querySelector('.temp')
+
+
+const deactivationPlayer = () => {
+  temp.style.display = 'none'
+  playerBtn.forEach(item => item.classList.remove('active'))
+  playeBlock.forEach(item => item.classList.remove('active'))
+}
+
+
+playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
+  deactivationPlayer()
+  btn.classList.add('active')
+  playeBlock[i].classList.add('active')
+}))
+
+
+radioPlayerInit()
+musicPlayerInit()
+videoPlayerInit()
