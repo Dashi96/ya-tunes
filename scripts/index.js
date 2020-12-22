46302f900ef9b2fa5c4c0fc175f37edf13fce905
Dashi <68922046+Dashi96@users.noreply.github.com>
2020-12-22 22:11:20 +0700

    create video player

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,26 @@ import { radioPlayerInit } from './radioPlayer.js'
 import { audioPlayerInit } from './audioPlayer.js'
 import { videoPlayerInit } from './videoPlayer.js'
 
+//get items sections from document
 const playerBtn = document.querySelectorAll('.player-btn')
 const playeBlock = document.querySelectorAll('.player-block')
 const temp = document.querySelector('.temp')
 
-
+//make the desired block of the player inactive when click
 const deactivationPlayer = () => {
   temp.style.display = 'none'
   playerBtn.forEach(item => item.classList.remove('active'))
   playeBlock.forEach(item => item.classList.remove('active'))
 }
 
-
+//make the desired block of the player active when click
 playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
   deactivationPlayer()
   btn.classList.add('active')
   playeBlock[i].classList.add('active')
 }))
 
-
-radioPlayerInit()
-audioPlayerInit()
+//call function all players
 videoPlayerInit()
+audioPlayerInit()
+radioPlayerInit()
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,72 @@
 export const videoPlayerInit = () => {
-  console.log('Video Init');
+
+  //get need items for Video Player
+  const videoPlayer = document.querySelector('.video-player')
+  const videoButtonPlay = document.querySelector('.video-button__play')
+  const videoButtonStop = document.querySelector('.video-button__stop')
+  const videoProgress = document.querySelector('.video-progress')
+  const videoTimePassed = document.querySelector('.video-time__passed')
+  const videoTimeTotal = document.querySelector('.video-time__total')
+
+
+  //add animating buttons
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove('fa-pause')
+      videoButtonPlay.classList.add('fa-play')
+    } else {
+      videoButtonPlay.classList.add('fa-pause')
+      videoButtonPlay.classList.remove('fa-play')
+    }
+  }
+
+  const togglePlay = () => {
+    if (videoPlayer.paused) {
+      videoPlayer.play()
+    } else {
+      videoPlayer.pause()
+    }
+  }
+
+  const stopPlay = () => {
+    videoPlayer.pause()
+    videoPlayer.currentTime = 0
+  }
+
+  //add zero before seconds
+  const addZero = n => n < 10 ? '0' + n : n
+
+  //add event listener for buttons
+  videoPlayer.addEventListener('click', togglePlay)
+  videoButtonPlay.addEventListener('click', togglePlay)
+
+  videoPlayer.addEventListener('play', toggleIcon)
+  videoPlayer.addEventListener('pause', toggleIcon)
+
+  videoButtonStop.addEventListener('click', stopPlay)
+
+  //we implement the functionality of the time counter
+  videoPlayer.addEventListener('timeupdate', () => {
+    const currentTime = videoPlayer.currentTime
+    const duration = videoPlayer.duration
+
+    videoProgress.value = (currentTime / duration) * 100
+
+    let minutePassed = Math.floor(currentTime / 60)
+    let secondsPassed = Math.floor(currentTime % 60)
+
+    let minuteTotal = Math.floor(duration / 60)
+    let secondsTotal = Math.floor(duration % 60)
+
+    videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`
+    videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`
+  })
+
+  //range input functionality
+  videoProgress.addEventListener('change', () => {
+    const duration = videoPlayer.duration
+    const value = videoProgress.value
+
+    videoPlayer.currentTime = (value * duration) / 100
+  })
 }
